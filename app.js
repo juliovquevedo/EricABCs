491f88aadcbfbe0baaef6fd6bcbac7adeda3b4d1
Julio Quevedo <juliovquevedo@gmail.com>
2018-09-26 19:56:52 -0400

    added arrays of numbers and letters

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 window.onload = function () {
     var colors = ['red', 'blue', 'pink', 'orange', 'purple', 'white', 'black', 'gray', 'violet'];
-    console.log(colors.length);
+    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    
     document.getElementById("box").addEventListener('click', change);
     var counter = 1;
     
