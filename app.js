26c7694b1994c53ed67dec314c029dd1ba417c47
Julio Quevedo <juliovquevedo@gmail.com>
2018-09-25 21:24:01 -0400

    populated the array with more colors, helped by Brooke

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 window.onload = function () {
-    var colors = ['red', 'blue'];
+    var colors = ['red', 'blue', 'pink', 'orange', 'purple', 'white', 'black', 'gray', 'violet'];
+    console.log(colors.length);
     document.getElementById("box").addEventListener('click', change);
+    var counter = 1;
     
     function change(e) {
         var target = document.getElementById("box");
-        target.style.background = colors[1];
+        target.style.background = colors[counter++];
     }
 }
